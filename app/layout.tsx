d933a58ab2f1b7652330565ee5ac7b1c661d86d4
Tyoma <raskolnikoffweb@mail.ru>
2024-09-28 01:21:55 +0500

    started working on Header and Container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { Header } from "../components/shared/Header";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -21,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <main className="min-h-screen">{children}</main>
+        <main className="min-h-screen">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,14 +1,27 @@
 import { cn } from "@/lib/utils";
 import React from "react";
+import { Container } from "./Container";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 interface Props {
   className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
-  return (
-    <header className={cn("border border-b", className)}>
-      <div></div>
-    </header>
-  );
-};
+export const Header: React.FC<Props> = ({ className }) => (
+  <header className={cn("border border-b", className)}>
+    <Container className="flex items-center justify-between py-8">
+      <div className="flex items-center gap-4">
+        <Image src="/logo.png" alt="logo" width={35} height={35} />
+        <div className="ml-4">
+          <h1 className="text-2xl font-black uppercase">Next Pizza</h1>
+          <p className="text-sm text-gray-400 leading-3">Вкусней уже некуда</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <Button variant="outline">Войти</Button>
+      </div>
+    </Container>
+  </header>
+);
